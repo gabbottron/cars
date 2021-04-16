8b2be59ba10fc026ad2f29bb0d056a1254a67e01
sancodes <munsan14@gmail.com>
2021-04-15 21:55:51 -0700

    add routers boilerplate

--- src/main.go
+++ src/main.go
@@ -1,9 +1,50 @@
 package main
 
 import (
-	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("RUnning this first time! :)))))")
+	router := gin.Default()
+
+	log.Println(router)
+
+	//GET '/'  --> all cars
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hit the get route",
+		})
+	})
+
+	//POST '/cars'  --> create cars
+	router.POST("/cars", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hit the POST cars route",
+		})
+	})
+
+	//GET '/cars/:carid'  --> get single car
+	router.GET("/cars/:carid", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hit the  GET single car route",
+		})
+	})
+
+	//PUT '/cars/:carid'  --> modify that single car
+	router.PUT("/cars/:carid", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hit the PUT single car route",
+		})
+	})
+
+	//DELETE '/cars/:carid'  --> delete that single car
+	router.DELETE("/cars/:carid", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hit the DELETE single car route",
+		})
+	})
+
+	router.Run()
 }
